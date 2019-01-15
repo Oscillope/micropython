18e4389ea4dcc5efda2a6a96414c15fe71f76693
Jason Rosenman <jvrosenman@gmail.com>
2019-01-14 20:52:50 -0500

    Default to using bit-banging method, but allow selecting DMA in the constructor

--- ports/esp32/espneopixel.c
+++ ports/esp32/espneopixel.c
@@ -13,7 +13,7 @@
 #define MAX_TRANSFER_SIZE 3*3*256         //3 spi-bits per bit * 3 color bytes * 256 leds
 
 static spi_device_handle_t spi;
-static uint8_t* pixBuf;
+static uint8_t* pixBuf = NULL;
 
 // This table generated based on the algorithm at:
 // https://www.rogerclark.net/arduino-stm32-neopixels-ws2812b-using-spi-dma/
@@ -82,13 +82,58 @@ void IRAM_ATTR esp_neopixel_init(uint8_t pin, uint8_t timing) {
 }
 
 void IRAM_ATTR esp_neopixel_write(uint8_t pin, uint8_t *pixels, uint32_t numBytes, uint8_t timing) {
-    int i = 0;
-    for (i = 0; i < numBytes; i++) {
-        pixBuf[i * 3] = spi_encode[*(pixels + i)] & 0x00ffffff;
+    if (pixBuf) {
+        // Use DMA transfer
+        int i = 0;
+        for (i = 0; i < numBytes; i++) {
+            pixBuf[i * 3] = spi_encode[*(pixels + i)] & 0x00ffffff;
+        }
+        struct spi_transaction_t trans = (struct spi_transaction_t){
+            .length = numBytes * 3,
+            .tx_buffer = pixBuf
+        };
+        spi_device_queue_trans(spi, &trans, 0);
+    } else {
+         // Use bit-banging method
+        uint8_t *p, *end, pix, mask;
+        uint32_t t, time0, time1, period, c, startTime, pinMask;
+
+        pinMask   = 1 << pin;
+        p         =  pixels;
+        end       =  p + numBytes;
+        pix       = *p++;
+        mask      = 0x80;
+        startTime = 0;
+
+        uint32_t fcpu = ets_get_cpu_frequency() * 1000000;
+
+        if (timing == 1) {
+            // 800 KHz
+            time0 = (fcpu * 0.35) / 1000000; // 0.35us
+            time1 = (fcpu * 0.8) / 1000000; // 0.8us
+            period = (fcpu * 1.25) / 1000000; // 1.25us per bit
+        } else {
+            // 400 KHz
+            time0 = (fcpu * 0.5) / 1000000; // 0.35us
+            time1 = (fcpu * 1.2) / 1000000; // 0.8us
+            period = (fcpu * 2.5) / 1000000; // 1.25us per bit
+        }
+
+        uint32_t irq_state = mp_hal_quiet_timing_enter();
+        for (t = time0;; t = time0) {
+            if (pix & mask) t = time1;                                  // Bit high duration
+            while (((c = mp_hal_ticks_cpu()) - startTime) < period);    // Wait for bit start
+            GPIO_REG_WRITE(GPIO_OUT_W1TS_REG, pinMask);                 // Set high
+            startTime = c;                                              // Save start time
+            while (((c = mp_hal_ticks_cpu()) - startTime) < t);         // Wait high duration
+            GPIO_REG_WRITE(GPIO_OUT_W1TC_REG, pinMask);                 // Set low
+            if (!(mask >>= 1)) {                                        // Next bit/byte
+                if(p >= end) break;
+                pix  = *p++;
+                mask = 0x80;
+            }
+        }
+        while ((mp_hal_ticks_cpu() - startTime) < period); // Wait for last bit
+        mp_hal_quiet_timing_exit(irq_state);
     }
-    struct spi_transaction_t trans = (struct spi_transaction_t){
-        .length = numBytes * 3,
-        .tx_buffer = pixBuf
-    };
-    spi_device_queue_trans(spi, &trans, 0);
 }
